cddb540b27441ff62b0da3adf5c380463e4893d2
Christophe Deneuve <33314621+christopheOclock@users.noreply.github.com>
2021-06-23 16:36:10 +0200

    Initial commit

---
+++ 02-Challenge-Assurances/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>O'ssurance</title>
+	</head>
+	<body>
+		<?php
+
+		// 1. Créer un formulaire HTML demandant les informations nécessaires au calcul
+		// 2. Après soumission du formulaire, créer les variables qui vont contenir les informations du client
+		// 3. Écrire le script qui permet de déterminer à quel palier peut prétendre un client, selon ses informations (et donc à quelle couleur de tarif)
+		// 4. Afficher le résultat, par ex. "Votre client a droit au tarif Vert"
+		// Bonus 1. Afficher le résultat de trois manières différentes : via `if` & `elseif` ou bien `switch` ou bien `array()`
+		// Bonus 2. fioritures graphiques
+
+		?>
+
+		<h1>O'ssurance</h1>
+		<h2>Calcul du tarif de votre client</h2>
+
+		<p>Votre client à droit au tarif <strong>xxx</strong></p>
+
+	</body>
+</html>
---
+++ README.md
@@ -0,0 +1,8 @@
+# Challenges PHP
+
+On s'attaque à l'envoi de données grâce aux formulaires HTML et à leur récupération en PHP.
+
+Pour cela, 2 challenges !
+
+- [O'to école](01-Challenge-Permis/README.md)
+- [O'ssurances](02-Challenge-Assurances/README.md)
